7a0e7e55fe49d36b769f08342294c5f60a808d57
Jilludumudi Ravikanth -X (jravikan - MINDTREE LIMITED at Cisco) <jravikan@cisco.com>
2020-12-08 13:11:47 +0530

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, g
 import sqlite3
+from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
@@ -9,7 +9,7 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factor = sqlite3.Row
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, value=(), commit=False, single=False):
@@ -19,6 +19,7 @@ def execute_sql(sql, value=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+
     cursor.close()
     return results
 
